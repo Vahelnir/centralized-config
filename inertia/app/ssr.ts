285709e6461582016f9598ccb429caf0c12f0585
vhamayon <vhamayon@knowmore.fr>
2024-12-09 10:12:55 +0100

    revert: stop using adonis' env in the ssr.ts file

--- inertia/app/ssr.ts
+++ inertia/app/ssr.ts
@@ -1,4 +1,3 @@
-import env from '#start/env'
 import { createInertiaApp } from '@inertiajs/vue3'
 import { TuyauPlugin } from '@tuyau/inertia/vue'
 import { renderToString } from '@vue/server-renderer'
@@ -15,9 +14,10 @@ export default function render(page: any) {
     },
 
     setup({ App, props, plugin }) {
-      const baseUrl = env.get('APP_URL')
-      if (!baseUrl) {
-        throw new Error('APP_URL is not set in the environment variables')
+      const baseUrl = props.initialPage?.props?.APP_URL
+      console.log('SSR APP_URL', baseUrl)
+      if (!baseUrl || typeof baseUrl !== 'string') {
+        throw new Error("APP_URL is missing from the initial page's props, or is not a string")
       }
 
       return createSSRApp({ render: () => h(App, props) })
